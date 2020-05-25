cc9150ec39a022dd3c0d426364d6f355ccbdf9fc
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-05-25 12:09:11 +0200

    add splash scene, add start scene, add level loader, add some image and music assets

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float splashDuration = 3f;
+    int currentSceneIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadStartScene());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator LoadStartScene()
+    {
+        yield return new WaitForSeconds(splashDuration);
+        SceneManager.LoadScene("StartScene");
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
