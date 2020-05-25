84877cb1ca504e2d7074da6a544f4a4752784b99
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-05-25 19:15:59 +0200

    add attacker movement

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class Attacker : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField][Range(0f, 5f)] float moveSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        gameObject.transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+    }
+}
