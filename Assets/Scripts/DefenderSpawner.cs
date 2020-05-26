183d302c0a1e5f67b423db0ca1740b7e99f551de
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-05-26 17:08:44 +0200

    add play grid area, add defender spawn with mouse click on play area

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defenderPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Mouse downclick on area");
+        SpawnDefender();
+    }
+
+    private void SpawnDefender()
+    {
+        var defenderObj = Instantiate(defenderPrefab, transform.position, transform.rotation);
+    }
+}
