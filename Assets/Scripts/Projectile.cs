4f081f132e7756259d91f6eadcf2679b00ac8389
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-05-26 12:44:28 +0200

    add projectile behaviour (movement)

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileRotationSpeed = 90f;
+    //Vector3 actualRotation = new Vector3(0,0,0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        //actualRotation += new Vector3(0, 0, projectileRotationSpeed);
+        //transform.RotateAround(transform.localPosition,
+        //                        Vector3.forward,
+        //                        projectileRotationSpeed * Time.deltaTime);
+        transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
+    }
+}
