36ebe927e639b2c7820916de14a14e382c7eb49f
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-05-28 11:38:14 +0200

    add defender selection ui

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach(DefenderButton defenderButton in buttons){
+            defenderButton.GetComponent<SpriteRenderer>().color = new Color32(106, 106, 106, 255);
+        }
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
