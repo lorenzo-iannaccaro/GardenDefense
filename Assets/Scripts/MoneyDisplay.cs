7c053f5ef489147cfdfb0f33a9676080dade7189
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-05-28 14:10:28 +0200

    add text to display and update the money count

---
+++ Assets/Scripts/MoneyDisplay.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyDisplay : MonoBehaviour
+{
+    [SerializeField] int money = 100;
+    Text moneyText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moneyText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateDisplay()
+    {
+        moneyText.text = money.ToString();
+    }
+
+    public void AddMoney(int moneyToAdd)
+    {
+        money += moneyToAdd;
+        UpdateDisplay();
+    }
+
+    public void SpendMoney(int moneySpent)
+    {
+        if(money >= 0)
+        {
+            money -= moneySpent;
+            if(money < 0)
+            {
+                money = 0;
+            }
+            UpdateDisplay();
+        }
+    }
+}
