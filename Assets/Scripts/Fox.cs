e06b8736c868b43bb89a4b53216c682174aa87bc
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-06-02 20:17:16 +0200

    add fox attacker to game, refactor animations directory

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        if (otherCollider.GetComponent<Defender>())
+        {
+            if (otherCollider.GetComponent<Gravestone>())
+            {
+                this.GetComponent<Animator>().SetTrigger("jump");
+            }
+            else
+            {
+                this.GetComponent<Attacker>().Attack(otherCollider.GetComponent<Defender>());
+            }
+            
+        }
+    }
+
+}
