742693b7b17a2685ebde106812ccde1c0d652594
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-06-04 10:04:09 +0200

    add options controller, update game music with single music player, update volume option slider

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    float defaultVolume = 0.75f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefsController.GetGameVolume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MusicPlayer musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (!musicPlayer) return;
+        musicPlayer.SetVolume(volumeSlider.value);
+    }
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsController.SetGameVolume(volumeSlider.value);
+        FindObjectOfType<LevelLoader>().LoadMainMenuScene();
+    }
+
+    public void SetDefaultValues()
+    {
+        volumeSlider.value = defaultVolume;
+    }
+}
